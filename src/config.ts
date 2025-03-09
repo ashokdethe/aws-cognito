b8a39f30a2863d1485ee395cc431e5efc63783ab
ashok dethe <ashokdethe123@gmail.com>
2025-03-09 18:38:53 +0530

    chnage in config file

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const awsConfig = {
     userPoolWebClientId: '7f1ga34l1giudj579f6dnnpc6o', // Replace with your App Client ID
     region: 'ap-south-1', // Replace with your AWS region (e.g., us-east-1)
     oauth: {
-        domain: "auth.demo.ai",
+        domain: "auth.mailio.ai",
         scope: ["openid", "email", "profile"],
         redirectSignIn: "http://localhost:3000",
         redirectSignOut: "http://localhost:3000",
